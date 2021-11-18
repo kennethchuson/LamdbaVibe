ccd4b2a3e6f21584ad61027870c4d8e7097d602e
Mitchel Baker <mitchrpbaker@gmail.com>
2021-11-17 22:50:05 -0800

    Made a initial progress creating frets/notes for ukulele; mvp of visualizer for now

--- client/src/instruments/mitchthebaker.tsx
+++ client/src/instruments/mitchthebaker.tsx
@@ -4,6 +4,7 @@ import * as Tone from 'tone';
 import classNames from 'classnames';
 import { List, Range } from 'immutable';
 import React from 'react';
+import "../css/ukulele.css";
 
 // project imports
 import { Instrument, InstrumentProps } from '../Instruments';
@@ -12,21 +13,19 @@ import { Instrument, InstrumentProps } from '../Instruments';
  * Contains implementation of components for Piano.
  ** ------------------------------------------------------------------------ */
 
-interface PianoKeyProps {
+interface UkuleleFretProps {
   note: string; // C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B
   duration?: string;
   synth?: Tone.Synth; // Contains library code for making sound
   minor?: boolean; // True if minor key, false if major key
-  octave: number;
   index: number; // octave + index together give a location for the piano key
 }
 
-export function PianoKey({
+export function UkuleleFret({
   note,
   synth,
-  minor,
   index,
-}: PianoKeyProps): JSX.Element {
+}: UkuleleFretProps): JSX.Element {
   /**
    * This React component corresponds to either a major or minor key in the piano.
    * See `PianoKeyWithoutJSX` for the React component without JSX.
@@ -36,7 +35,13 @@ export function PianoKey({
     // 1. The JSX refers to the HTML-looking syntax within TypeScript.
     // 2. The JSX will be **transpiled** into the corresponding `React.createElement` library call.
     // 3. The curly braces `{` and `}` should remind you of string interpolation.
-    <div
+    <div className="ukulele-fret">
+    
+    </div>
+  );
+}
+
+/*<div
       onMouseDown={() => synth?.triggerAttack(`${note}`)} // Question: what is `onMouseDown`?
       onMouseUp={() => synth?.triggerRelease('+0.25')} // Question: what is `onMouseUp`?
       className={classNames('ba pointer absolute dim', {
@@ -51,70 +56,27 @@ export function PianoKey({
         width: minor ? '1.5rem' : '2rem',
         marginLeft: minor ? '0.25rem' : 0,
       }}
-    ></div>
-  );
-}
-
-// eslint-disable-next-line
-function PianoKeyWithoutJSX({
-  note,
-  synth,
-  minor,
-  index,
-}: PianoKeyProps): JSX.Element {
-  /**
-   * This React component for pedagogical purposes.
-   * See `PianoKey` for the React component with JSX (JavaScript XML).
-   */
-  return React.createElement(
-    'div',
-    {
-      onMouseDown: () => synth?.triggerAttack(`${note}`),
-      onMouseUp: () => synth?.triggerRelease('+0.25'),
-      className: classNames('ba pointer absolute dim', {
-        'bg-black black h3': minor,
-        'black bg-white h4': !minor,
-      }),
-      style: {
-        top: 0,
-        left: `${index * 2}rem`,
-        zIndex: minor ? 1 : 0,
-        width: minor ? '1.5rem' : '2rem',
-        marginLeft: minor ? '0.25rem' : 0,
-      },
-    },
-    [],
-  );
-}
-
-function PianoType({ title, onClick, active }: any): JSX.Element {
-  return (
-    <div
-      onClick={onClick}
-      className={classNames('dim pointer ph2 pv1 ba mr2 br1 fw7 bw1', {
-        'b--black black': active,
-        'gray b--light-gray': !active,
-      })}
-    >
-      {title}
-    </div>
-  );
-}
+    ></div>*/
 
 function Ukulele({ synth, setSynth }: InstrumentProps): JSX.Element {
-  const keys = List([
-    { note: 'C', idx: 0 },
-    { note: 'Db', idx: 0.5 },
-    { note: 'D', idx: 1 },
-    { note: 'Eb', idx: 1.5 },
-    { note: 'E', idx: 2 },
-    { note: 'F', idx: 3 },
-    { note: 'Gb', idx: 3.5 },
-    { note: 'G', idx: 4 },
-    { note: 'Ab', idx: 4.5 },
-    { note: 'A', idx: 5 },
-    { note: 'Bb', idx: 5.5 },
-    { note: 'B', idx: 6 },
+  //const keys = List([
+  //  { note: 'C', idx: 0 },
+  //  { note: 'Db', idx: 0.5 },
+  //  { note: 'D', idx: 1 },
+  //  { note: 'Eb', idx: 1.5 },
+  //  { note: 'E', idx: 2 },
+  //  { note: 'F', idx: 3 },
+  //  { note: 'Gb', idx: 3.5 },
+  //  { note: 'G', idx: 4 },
+  //  { note: 'Ab', idx: 4.5 },
+  //  { note: 'A', idx: 5 },
+  //  { note: 'Bb', idx: 5.5 },
+  //  { note: 'B', idx: 6 },
+  //]);
+  const frets = List([
+    { note: 'Ab', idx: 0 }, { note: 'Db', idx: 1 }, { note: 'F', idx: 2 }, { note: 'Bb', idx: 3 },
+    { note: 'A', idx: 4 }, { note: 'D', idx: 5 }, { note: 'Gb', idx: 6 }, { note: 'B', idx: 7 },
+    { note: 'Bb', idx: 8 }, { note: 'Eb', idx: 9 }, { note: 'G', idx: 10 }, { note: 'C', idx: 11 },
   ]);
 
   const setOscillator = (newType: Tone.ToneOscillatorType) => {
@@ -142,8 +104,25 @@ function Ukulele({ synth, setSynth }: InstrumentProps): JSX.Element {
 
   return (
     <div className="pv4">
-      <div className="relative dib h4 w-100 ml4">
-        {Range(2, 7).map(octave =>
+      <div className="relative dib h4 w-100 ml4 ukulele">
+        {
+          Range(1, 12).map(row => (
+            <div className="fret">
+              {frets.map(fret => {
+                const note = `${fret.note}`;
+
+                return (
+                  <UkuleleFret 
+                    note={note}
+                    synth={synth}
+                    index={fret.idx}
+                  />
+                );
+              })}
+            </div>
+          ))
+        }
+        {/*Range(2, 7).map(octave =>
           keys.map(key => {
             const isMinor = key.note.indexOf('b') !== -1;
             const note = `${key.note}${octave}`;
@@ -158,9 +137,10 @@ function Ukulele({ synth, setSynth }: InstrumentProps): JSX.Element {
               />
             );
           }),
-        )}
+        )*/}
       </div>
-      <div className={'pl4 pt4 flex'}>
+
+      {/*<div className={'pl4 pt4 flex'}>
         {oscillators.map(o => (
           <PianoType
             key={o}
@@ -169,7 +149,7 @@ function Ukulele({ synth, setSynth }: InstrumentProps): JSX.Element {
             active={synth?.oscillator.type === o}
           />
         ))}
-      </div>
+        </div>*/}
     </div>
   );
 }
--- client/src/visualizers/mitchthebaker.tsx
+++ client/src/visualizers/mitchthebaker.tsx
@@ -14,20 +14,22 @@ interface MyInterface {
   data: Record<string, Item>;
 }
 
+const particles: Particle[] = [];
+
 class Particle {
 
   public position;
   public velocity;
   public acc;
   public w; 
-  
+  public color: any[];
 
   constructor(p5: P5) {
-    this.position = P5.Vector.random2D().mult(250);
-    this.velocity = P5.Vector.random2D().mult(0);
-    //p5.createVector(0, 0);
+    this.position = Object.getPrototypeOf(p5).constructor.Vector.random2D().mult(250);
+    this.velocity = p5.createVector(0, 0);
     this.acc = this.position.copy().mult(p5.random(0.001, 0.0001));
     this.w = p5.random(3, 5);
+    this.color = [p5.random(200, 255), p5.random(200, 255), p5.random(200, 255)];
   }
 
   update(): void {
@@ -47,7 +49,7 @@ class Particle {
 
   show(p5: P5): void {
     p5.noStroke()
-    p5.fill(255);
+    p5.fill(this.color);
     p5.ellipse(this.position.x, this.position.y, this.w);
   }
 }
@@ -60,13 +62,17 @@ export const mitchthebakerVisualizer = new Visualizer(
     const dim = Math.min(width, height);
     const frameCount = p5.frameCount;
 
-    p5.angleMode(p5.DEGREES);
+    //p5.image(p5.loadImage(mandelbrot_set), 0, 0, width, height);
+    //p5.image(image, 0, 0, width, height);
+
     p5.background(0);
     p5.stroke(255);
+    p5.strokeWeight(3);
     p5.noFill();
     p5.translate(width / 2, height / 2);
-
+    
     const wave = analyzer.getValue();
+    
 
     for(let t = -1; t <= 1; t += 2) {
       p5.beginShape();
@@ -87,66 +93,17 @@ export const mitchthebakerVisualizer = new Visualizer(
       p5.endShape();
     }
 
-    const particles: Particle[] = [];
-
     let p: Particle = new Particle(p5);
     particles.push(p);
-    //console.log(particles);
 
     for(let i = particles.length - 1; i >= 0; i--) {
       if(!particles[i].edges(width, height)) {
-        //particles[i].update();
-        //particles[i].show(p5);
-
-        p5.noStroke()
-        p5.fill(255);
-        p5.ellipse(particles[i].position.x, particles[i].position.y, particles[i].w);
+        particles[i].update();
+        particles[i].show(p5);
       }
       else {
-        particles.splice(i, i);
+        particles.splice(i, 1);
       }
-      
     }
-
-    /*
-    p5.beginShape();
-    
-    //p5.background(250);
-    
-
-    //p5.strokeWeight(dim * 0.01);
-    //p5.stroke(255, 255, 255, 255);
-    //p5.noFill();
-
-    p5.push();
-
-    
-
-    for(let i = 0; i < 5; i++) {
-
-      p5.push();
-
-      for(let j = 0; j < values.length; j++) {
-
-        const freq = values[j] as number;
-
-        p5.translate(
-          p5.sin(frameCount * 0.001 + i) * 100,
-          p5.tan(frameCount * 0.001 + i) * 100,
-          j * freq
-        );
-        
-        p5.rotateZ(frameCount * 0.1);
-        p5.push();
-        p5.sphere(8, 6, 4);
-        p5.pop();
-      }
-      p5.pop();
-    }
-
-    p5.pop();
-
-    p5.endShape();
-    */
   },
 );
